e9c4391e759e4ccec9b9a0a96a14d3b6a207c6be
Joaquin Leimeter <joaquin@Joaquins-MacBook-Pro.local>
2022-12-05 10:03:41 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/pages/create_post.dart';
+import 'package:instagram_clone/pages/home.dart';
+import 'package:instagram_clone/pages/notifications.dart';
+import 'package:instagram_clone/pages/profile.dart';
+import 'package:instagram_clone/pages/search.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final pages = const [
+    Home(),
+    Search(),
+    CreatePost(),
+    Notifications(),
+    Profile(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 5,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: const Text('Instagram')),
+          body: TabBarView(children: pages),
+          bottomNavigationBar: const TabBar(
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.black,
+            indicatorColor: Colors.transparent,
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.search)),
+              Tab(icon: Icon(Icons.add)),
+              Tab(icon: Icon(Icons.favorite)),
+              Tab(icon: Icon(Icons.person)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
